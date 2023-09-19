c381281046621cfbbbe881bbfaddd8dacfe1322e
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-19 11:12:47 +0200

    feat: Expose requests and responses

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,7 @@ export * from './Api.js';
 export * from './Data.js';
 export * from './types.js';
 export { Manifest } from './gen/manifest.js';
+export { AppRequest, ViewRequest, ListenerRequest, ResourceRequest } from './gen/request.js';
+export { JsonViewResponse, LenraViewResponse } from './gen/response.js';
 export * from './components/index.js';
 export * from './styles/index.js';
