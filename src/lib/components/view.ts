71ef35b68714808b8085c7f8345c533f880e7b22
jonas.martinez <cbobbxta.fr@gmail.com>
2024-02-15 16:12:37 +0100

    feat: Add options to find

--- src/lib/components/view.ts
+++ src/lib/components/view.ts
@@ -17,10 +17,19 @@ export function View(name: ViewName): ViewImpl {
 
 export class ViewImpl extends ViewBaseImpl {
   // Add here custom implementations
-  find<T extends Data>(coll: Class<T>, query: Query, projection?: { [k: string]: unknown }): this
-  find(coll: string, query: Query, projection?: { [k: string]: unknown }): this
+  find<T extends Data>(coll: Class<T>, query: Query, projection?: { [k: string]: unknown }, options?: {
+    limit?: number | null;
+    skip?: number | null;
+  }): this
+  find(coll: string, query: Query, projection?: { [k: string]: unknown }, options?: {
+    limit?: number | null;
+    skip?: number | null;
+  }): this
   find(find: IView['find']): this
-  find(param1: string | Class<Data> | IView['find'], query?: Query, projection?: { [k: string]: unknown }): this {
+  find(param1: string | Class<Data> | IView['find'], query?: Query, projection?: { [k: string]: unknown }, options?: {
+    limit?: number | null;
+    skip?: number | null;
+  }): this {
     if (typeof param1 === "function") {
       param1 = DataApi.collectionName(<Class<Data>>param1);
     }
